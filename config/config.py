c8430192a084c1acbd3d31005164c5e05d1ce852
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-24 11:23:43 +0530

    add llm

--- config/config.py
+++ config/config.py
@@ -1,3 +1,6 @@
+import streamlit as st
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_groq.chat_models import ChatGroq
 import yaml
 
 def get_congig():
@@ -12,7 +15,21 @@ def get_congig():
         st.error("🚨 Error while loading config: " + str(e.args))
 
 def get_embeddings():
-    pass
+    try:
+        model_kwargs = {'device': 'cpu'}
+        encode_kwargs = {'normalize_embeddings': True}
+        
+        return HuggingFaceEmbeddings(
+            model_name=get_congig()["embeddings"]["name"], 
+            model_kwargs=model_kwargs, 
+            encode_kwargs=encode_kwargs
+            )
+    except Exception as e:
+        st.error("🚨 Error while loading embeddings: " + str(e.args))
 
 def get_llm():
-    pass
+    try:
+        llm = ChatGroq(model=get_congig()["llm"]["name"], temperature=get_congig()["llm"]["temperature"])
+        return llm
+    except Exception as e:
+        st.error("🚨 Error while loading llm: " + str(e.args))
