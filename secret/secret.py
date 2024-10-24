e2088548453ddb0561c48ab9599004c94972f0b0
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-24 10:50:40 +0530

    add header

--- secret/secret.py
+++ secret/secret.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 
 
-def load_secrets(groq_api_key: str=None)-> None:
+def load_secrets(groq_api_key=None)-> None:
     try:
         if "GROQ_API_KEY" in st.secrets["GROQ_API_KEY"]:
             os.environ["GROQ_API_KEY"] = st.secrets["GROQ_API_KEY"]
